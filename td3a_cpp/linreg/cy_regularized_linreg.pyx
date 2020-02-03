a402217ec242af61ab8b9273ad2742b1d2eb5ceb
Axeldnahcram <axel.marchand@ensae.fr>
2020-02-03 22:43:37 +0100

    FIX - fixed issue with cimport

--- td3a_cpp/linreg/cy_regularized_linreg.pyx
+++ td3a_cpp/linreg/cy_regularized_linreg.pyx
@@ -1,10 +1,10 @@
 # distutils: language = c++
 # distutils: sources = td3a_cpp/linreg/cy_regularized_linreg_.cpp
 
-cimport cy_regularized_linreg_
+cimport cy_regularized_linreg
 
 def py_regularized_linreg(double[::1] data_x, double[::1] data_y, int num_samples, int num_features, 
                           double[::1] beta, double alpha, double L1_ratio, int max_iter, double tol):
     
-    cy_regularized_linreg_.coord_desc(&data_x[0], &data_y[0], num_samples, num_features, 
+    cy_regularized_linreg.coord_desc(&data_x[0], &data_y[0], num_samples, num_features, 
                                                  &beta[0], alpha, L1_ratio, max_iter, tol)
